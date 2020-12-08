07079f91fd8d795029710136c7543fb5097a4a68
Brian Gardner <1761566+briangardner@users.noreply.github.com>
2020-12-07 19:33:26 -0500

    Initial setup.  Function runs.

---
+++ Test.Functions/Test.Functions/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Test.Functions;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace Test.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            
+        }
+    }
+}
